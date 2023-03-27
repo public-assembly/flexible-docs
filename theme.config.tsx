83b8d7d9cad9ddd799e0abee1a0577ab7bc258c1
Valerie Andy <valdaraa@protonmail.com>
2023-03-27 19:58:28 -0400

    fix build error.

--- theme.config.tsx
+++ theme.config.tsx
@@ -108,7 +108,6 @@
          <link
            rel="preconnect"
            href="https://fonts.gstatic.com"
-           crossOrigin="true"
          />
          <link
            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"

64e22af77f3213363c68f22447c0810718c3df17
Valerie Andy <valdaraa@protonmail.com>
2023-03-27 18:32:54 -0400

    add favicon files

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,10 +3,12 @@ import Head from "next/head";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const Favicon: React.FC = () => {
-  const faviconPath = "/path/to/your/favicon.ico";
   return (
     <Head>
-      <link rel="icon" href={faviconPath} />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 };

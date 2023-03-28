0b55cfc47078b26f4fa5d532fbbe73091767aae5
Valerie Andy <valdaraa@protonmail.com>
2023-03-27 23:30:02 -0400

    fix: random doc fixes, favicon and try gif

--- components/table2.tsx
+++ components/table2.tsx
@@ -11,8 +11,8 @@ export function OptionTable2({ options }: { options: [ URL, any] }) {
         <table className="w-full border-collapse text-sm">
         <thead>
             <tr className="border-b py-4 text-left dark:border-neutral-700">
-            <th className="py-2 font-semibold">Hook</th>
-            <th className="py-2 px-6 font-semibold">Usage</th>
+            <th className="py-2 font-semibold"></th>
+            <th className="py-2 px-6 font-semibold"></th>
             </tr>
         </thead>
         <tbody className="align-baseline text-gray-900 dark:text-gray-100">
